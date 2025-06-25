c0d7f8ffaff74ce744d55b4d4747508593468434
theron88888 <theron.wk98@gmail.com>
2025-06-25 16:33:05 +0800

    Fix scraper

--- scraper.py
+++ scraper.py
@@ -22,11 +22,13 @@ MASTER_PATH = os.path.join(SAVE_DIR, "used_cars_master.csv")
 
 # ---------- SELENIUM SETUP ---------- #
 options = Options()
-options.add_argument('--headless')
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+options.add_argument('--headless=new')  # or '--headless=chrome'
 options.add_argument('--disable-gpu')
-options.add_argument('--log-level=3')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--window-size=1920,1080')
+options.add_argument(
+    '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36')
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 20)
 
