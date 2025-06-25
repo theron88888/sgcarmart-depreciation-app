6a4e5e2eed5944cd8de9ad14574dfb59fb12a91f
theron88888 <theron.wk98@gmail.com>
2025-06-25 16:59:11 +0800

    Fix scraper

--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ MASTER_PATH = os.path.join(SAVE_DIR, "used_cars_master.csv")
 
 # ---------- SELENIUM SETUP ---------- #
 options = Options()
-options.add_argument('--headless=new')
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
